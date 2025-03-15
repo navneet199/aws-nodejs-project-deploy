3a1526f3b0c9c3bd8ab6386a79ef90bb5216a87d
= <=nk28814@gmail.com>
2025-03-15 13:53:07 +0530

    Added user route

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/api/get', (req, res)=>{
     res.send({message:'Hello from Nodejs.'});
 });
 
+app.get('/api/users', (req, res)=>{
+    res.send({message:'Hello from Nodejs users.'});
+});
+
 app.listen(8000, ()=>{
     console.log('Listening to 8000');
 })
