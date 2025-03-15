e4480276de6fd9e40641b8ada9df32557f6b72a6
= <=nk28814@gmail.com>
2025-03-15 14:10:59 +0530

    added category routes

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.get('/api/products', (req, res)=>{
     res.send({message:'Hello from Nodejs products.'});
 });
 
+app.get('/api/categories', (req, res)=>{
+    res.send({message:'Hello from Nodejs categories.'});
+});
+
 app.listen(8000, ()=>{
     console.log('Listening to 8000');
 })
