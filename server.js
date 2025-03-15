9da327a71edbe776d100c5482cbe01ff0b2edab5
= <=nk28814@gmail.com>
2025-03-15 15:52:02 +0530

    Added password route

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 require("dotenv").config();
+const bcrypt = require('bcrypt');
 
 app.get('/api/get', (req, res)=>{
     res.send({message:'Hello from Nodejs.'});
@@ -17,6 +18,17 @@ app.get('/api/products', (req, res)=>{
 app.get('/api/categories', (req, res)=>{
     res.send({message:process.env.MYNAME});
 });
+
+
+app.get('/api/password', async(req, res)=>{
+    const password = "123456789";
+    const saltRounds = 10; // Adjust for security vs performance
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    // return hashedPassword;
+    res.send({message:hashedPassword});
+});
+
+
 
 app.listen(8000, ()=>{
     console.log('Listening to 8000 ok');
