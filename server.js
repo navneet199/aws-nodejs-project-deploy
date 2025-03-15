a16335e2deb175926ed81f656c69f9efbd2a1b22
= <=nk28814@gmail.com>
2025-03-15 14:06:35 +0530

    Added product Route

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/api/users', (req, res)=>{
     res.send({message:'Hello from Nodejs users.'});
 });
 
+app.get('/api/products', (req, res)=>{
+    res.send({message:'Hello from Nodejs products.'});
+});
+
 app.listen(8000, ()=>{
     console.log('Listening to 8000');
 })
